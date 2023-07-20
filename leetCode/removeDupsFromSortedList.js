23baddc5422cd5bded37107c1af76d12f9a73f11
ImNGodmode <bjames.56@icloud.com>
2023-07-19 20:42:25 -0400

    updated removeDupsFromSortedList.js to meet leetcode standards and solve the problem in the space complexity required.

--- leetCode/removeDupsFromSortedList.js
+++ leetCode/removeDupsFromSortedList.js
@@ -1,29 +1,16 @@
-let nums = [0,0,1,1,1,1,2,2,3,3,4,5];
+let nums = [0,0,1,1,1,2,2,3,3,4];
 
-//very clean but usesed a second array and the problem asks you to not use a second array 
+
 const removeDuplicates = (() => {
-let ray = new Set([...nums]);
-nums = ray
-console.log(nums)
+    let index = 0
+while (index <= (nums.length-1)){
+    if (!nums[index+1] && nums[index+1] !== 0) break;
+    if (nums[index] === nums[index+1]) nums.splice(index,1);
+    console.log(nums);
+    if (nums[index] !== nums[index+1])index++;
+
+}
 });
 
-// removeDuplicates()
-
-// const removeDuplicates = (() => {
-
-//      for (i=0; i<nums.length; i++) {
-//         // if (isNaN(nums[i]) ){
-//         //    // console.log(nums[i])
-//         //     nums.splice(i,1)
-//         //     //console.log(nums)
-//         // }
-//        if (nums[i]===nums[i+1]){
-//             nums.splice(i,1)
-//             i--
-//        }      
-//     }
-//     k = nums.length
-//     console.log(k)
-// });
 
 removeDuplicates()
